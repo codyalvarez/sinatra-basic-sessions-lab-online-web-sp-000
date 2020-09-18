b300f573f84174339b466090218ec3f00f6b8749
Cody Alvarez <cody_alvarez19@outlook.com>
2020-09-18 17:17:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
+require "pry"
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+
+    erb :index
+  end
+
+  post '/checkout' do
+    # binding.pry
+
+    @cart = params[:item][:purchase]
+    #
+    # params[:item][:purchase].each do |i|
+    #   @cart << i[:item]
+    # end
+    #
+    #
+    erb :checkout
+  end
 end
